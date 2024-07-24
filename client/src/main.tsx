d5bfa1579778882b51ffde4275311266c1c690d4
Falekin7 <falekinldg@gmail.com>
2024-07-24 11:45:58 -0300

    dashboard page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { routes } from './routes/routes.tsx'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './routes/routes.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={routes} />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={routes} />
+	</React.StrictMode>,
+);
--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,16 +1,16 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
-import Home from "../pages/home/Home";
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import DashboardPage from '../pages/dashboard';
 
 export const routes = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                element: <Home />,
-                index: true
-            }
-        ]
-    }
-])
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: 'dashboard',
+				element: <DashboardPage />,
+			},
+		],
+	},
+]);
